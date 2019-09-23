5924d52776b551ecfb860cccda909c3f821dade1
匡洋 <@Masahiro>
2019-09-23 22:12:19 +0900

    first commit of OOTA

--- src/main/java/com/tuyano/springboot/HeloController.java
+++ src/main/java/com/tuyano/springboot/HeloController.java
@@ -9,15 +9,15 @@ public class HeloController {
 
 	@RequestMapping("/")
 	public String index(Model model) {
-		 // 横軸
-        // ラベルです。String型の配列に、適当に値を入れておきます。
+		 // 
+        // 
         String label[] = {"a","b","c","d","e","f","g"};
 
-        // 縦軸
-        // 具体的な値です。int型で、この値も適当です。
-        int point[] = {5,3,7,1,8,3,4,};
+        // 
+        // 
+        int point[] = {5,1,7,6,15,3,4,};
 
-        // Modelに格納します。ビュー側でグラフ用の配列を受け取れるようにしておきます。
+        // 
         model.addAttribute("label",label);
         model.addAttribute("point",point);
 
